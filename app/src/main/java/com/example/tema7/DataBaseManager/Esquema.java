343fbfef7dc0f8f49d693f0fe85c77df40b7b766
jlsoriano86 <josesorna86@gmail.com>
2019-03-04 16:40:46 +0100

    Creación del paquete DataBaseManager #44

---
+++ app/src/main/java/com/example/tema7/DataBaseManager/Esquema.java
@@ -0,0 +1,29 @@
+package com.example.tema7.DataBaseManager;
+
+import android.provider.BaseColumns;
+
+public class Esquema {
+    public Esquema() {
+    }
+
+    public static abstract class Lugar implements BaseColumns {
+        public static final String TABLE_NAME = "LUGAR";
+
+        public static final String COLUMN_NAME_ID = "id";
+        public static final String COLUMN_NAME_NOMBRE = "nombre";
+        public static final String COLUMN_NAME_CATEGORIA = "categoria";
+        public static final String COLUMN_NAME_LONGITUD = "longitud";
+        public static final String COLUMN_NAME_LATITUD = "latitud";
+        public static final String COLUMN_NAME_VALORACION = "valoracion";
+        public static final String COLUMN_NAME_COMENTARIOS = "comentarios";
+
+        public static final String COLUMN_TYPE_ID = "INTEGER";
+        public static final String COLUMN_TYPE_NOMBRE = "TEXT";
+        public static final String COLUMN_TYPE_CATEGORIA = "TEXT";
+        public static final String COLUMN_TYPE_LONGITUD = "FLOAT";
+        public static final String COLUMN_TYPE_LATITUD = "FLOAT";
+        public static final String COLUMN_TYPE_VALORACION = "INTEGER";
+        public static final String COLUMN_TYPE_COMENTARIOS = "TEXT";
+
+    }
+}
