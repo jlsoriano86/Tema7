6b603c37ce8e00fd34937d8b959dd8606fabc19a
Jose Luis Soriano <35837723+jlsoriano86@users.noreply.github.com>
2019-03-05 10:17:08 +0100

    Pantalla Nuevo/Edición: Icono de guardado #34

--- app/src/main/java/com/example/tema7/EdicionActivity.java
+++ app/src/main/java/com/example/tema7/EdicionActivity.java
@@ -1,7 +1,13 @@
 package com.example.tema7;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -45,7 +51,30 @@ public class EdicionActivity extends AppCompatActivity {
         txtComentarios.setText(App.lugarActivo.getComentarios().toString());
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_edicion, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opcInsertar: clicInsertar(); break;
+
+        }
+        return true;
+    }
+
+    public void clicInsertar() {
+        App.accion = App.INSERTAR;
+        startActivity(new Intent(this, EdicionActivity.class));
+        finish();
+    }
+
+
+/*
     public void clicGuardar(View view) {
         String nombre = txtNombre.getText().toString();
         String categoria = spCategoria.getSelectedItem().toString();
@@ -68,16 +97,16 @@ public class EdicionActivity extends AppCompatActivity {
                 case App.INSERTAR:
                     LogicLugar.insertarLugar(this, App.lugarActivo);
                     break;
-                case App.EDITAR:
-                    LogicLugar.editarLugar(this, App.lugarActivo);
-                    break;
+
             }
             mostrarMensaje("Lugar " + nombre + " ha sido almacenado.");
             finish();
         }
     }
+*/
 
-    private void mostrarMensaje(String msg) {
+  /*  private void mostrarMensaje(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
+    */
 }
