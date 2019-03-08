4fd8aa40ca050f6d5a72502c006b274144d4a044
jlsoriano86 <josesorna86@gmail.com>
2019-03-08 19:58:08 +0100

    Pantalla Mapa: añadiendo marcadores de colores #11

--- app/src/main/java/com/example/tema7/MapsActivity.java
+++ app/src/main/java/com/example/tema7/MapsActivity.java
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -59,6 +60,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             for (Lugar l : listaLugares) {
                 Marcador = new LatLng(l.getLatitud(), l.getLongitud());
+                if (l.getCategoria() == 1){
+                    mMap.addMarker(new MarkerOptions().position(Marcador).title(l.getNombre()).icon(BitmapDescriptorFactory.fromResource(R.drawable.mi_marcador))););
+                }
 
             }
         }
